b573c9fed8df683b399e88aab24cd492bfdf1293
twomeetings <2326220@163.com>
2022-01-03 07:18:49 +0800

    test: add graphql json parse demo

--- src/myGraphql.test.js
+++ src/myGraphql.test.js
@@ -1,5 +1,20 @@
+import util from 'util'
 import myGraphql from './myGraphql'
+// import { graphql } from 'graphql'
 
 it('test my graphql ', () => {
-  expect(myGraphql).toBe(null)
+  const json = `query BlogPostBySlug($slug: String!) {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+  }`
+  const result = myGraphql.parse(json)
+
+  console.log(util.inspect(result, { color: true }))
+  // console.log(result)
+  expect(myGraphql.parse(json)).not.toBe(null)
+  // console.log(graphql)
+  // expect(graphql(json)).not.toBe(null)
 })
